cf56afba163b479c6023ff09d0d4e459b9dae669
trinidadjasmine <124627226+trinidadjasmine@users.noreply.github.com>
2024-01-27 23:32:35 -0600

    Add files via upload

---
+++ Week2_HW_2-4/Week2_HW_2-4/EndUserTest.cpp
@@ -0,0 +1,11 @@
+#include "RestaurantBill.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	RestaurantBill restaurantBill;
+	restaurantBill.calculate();
+	restaurantBill.display();
+
+	return 0;
+}
---
+++ Week2_HW_2-4/Week2_HW_2-4/RestaurantBill.cpp
@@ -0,0 +1,19 @@
+#include "RestaurantBill.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void RestaurantBill::calculate() {
+	mealCost = 44.50;
+	taxPercentage = 6.75 / 100;
+	tax = taxPercentage * mealCost;
+	tip = (mealCost + tax) * .15;
+	total = mealCost + tax + tip;
+}
+
+void RestaurantBill::display() {
+	cout << fixed << setprecision(2) << "Meal Cost:" << setw(5) << "$" << mealCost << endl;
+	cout << "Tax:" << setw(11) << "$" << tax << endl;
+	cout << "Tip:" << setw(11) << "$" << tip << endl;
+	cout << "Total:" << setw(9) << "$" << total << endl;
+} 
